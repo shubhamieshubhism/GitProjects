b2acd46740c2f69c3faaa69ce82bd71026ae4e99
techverito <shubham.tirpude@techverito.com>
2026-08-01 12:25:44 +0530

    Addition of itachi webpage and Modification

--- LogicBuilding/JAVA/SearchAlgorithms.java
+++ LogicBuilding/JAVA/SearchAlgorithms.java
@@ -38,28 +38,28 @@ public class SearchAlgorithms {
 
     }
 
-    public static int findMin(int[] nums){
+    public static int findMin(int[] nums) {
         int minVal = Integer.MAX_VALUE;
-        for(int i =0;i<nums.length;i++){
-            minVal=Math.min(minVal, nums[i]);
+        for (int i = 0; i < nums.length; i++) {
+            minVal = Math.min(minVal, nums[i]);
         }
         return minVal;
     }
 
-    public static int findMax(int[] nums){
+    public static int findMax(int[] nums) {
         int maxVal = -1;
-        for(int i =0;i<nums.length;i++){
-            maxVal=Math.max(maxVal, nums[i]);
+        for (int i = 0; i < nums.length; i++) {
+            maxVal = Math.max(maxVal, nums[i]);
         }
         return maxVal;
     }
 
-    public static int findRotations(int []arr){
+    public static int findRotations(int[] arr) {
         int n = arr.length;
-        int minVal=arr[0];
-        int minIndex=0;
-        for(int i =0;i<n;i++){
-            if(arr[i]< minVal){
+        int minVal = arr[0];
+        int minIndex = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < minVal) {
                 minVal = arr[i];
                 minIndex = i;
             }
@@ -67,9 +67,66 @@ public class SearchAlgorithms {
         return minIndex;
     }
 
+    public static int singleNonDuplicate(int[] arr) {
+        int n = arr.length;
+        // when the length of the array is 1
+        if (n == 1)
+            return arr[0];
+        // when the non duplicate is first or last
+        if (arr[0] != arr[1])
+            return arr[0];
+        if (arr[n - 1] != arr[n - 2])
+            return arr[n - 1];
+        // Binary seach for other element excluding the first and last
+        int low = 1;
+        int high = n - 2;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] != arr[mid + 1] && arr[mid] != arr[mid - 1])
+                return arr[mid];
+
+            if ((mid % 2 == 1 && arr[mid] != arr[mid - 1]) ||
+                    (mid % 2 == 0 && arr[mid] != arr[mid + 1])) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+
+        }
+        return -1;
+    }
+
+    public static int findPeakElement(int[] nums) {
+        int n = nums.length;
+        System.out.println(n);
+        for (int i = 0; i < n; i++) {
+            boolean left = (i == 0) || (nums[i] >= nums[i - 1]);
+            boolean right = (i == n - 1) || (nums[i] >= nums[i + 1]);
+            if (left && right)
+                return i;
+        }
+        return -1;
+    }
+
+    public static int findPeakElementV2(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
     public static void main(String[] args) {
-        int[] arr = { 3,4,5,6,7,0,1,2};
-        System.out.println(findRotations(arr));
+        int[] nums = { 1, 3, 20, 4, 1, 0 };
+        int index = findPeakElementV2(nums);
+        System.out.println("peak at index: " + index + " with value: " + nums[index]);
     }
 
 }
